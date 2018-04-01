262d45b96812655c6286075b1b36f5e72f10435a
mpandit3 <meelyn.pandit@gmail.com>
2018-04-01 18:14:04 -0500

    Added box number and time stamp to file name so audio files will not be over written

--- audio.sh
+++ audio.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-arecord -D plughw:1 -c1 -r 48000 -f S32_LE -t wav -V mono -d 10800 --max-file-time 1800 -v /mnt/usbdisk/rpi_audio.wav
+filename="box_num"_$(date +"%m%d%y-%H%M%S")
+
+arecord -D plughw:1 -c1 -r 48000 -f S32_LE -t wav -V mono -d 10 --max-file-time 1800 -v /home/pi/$filename.wav
