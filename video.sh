4d2ba73a60765e60f298a5b936462ea464bd3e32
mpandit3 <meelyn.pandit@gmail.com>
2018-04-01 18:12:59 -0500

    Added box number and time stamp to file name so files will not be over-written.

--- video.sh
+++ video.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+ #!/bin/bash
+
+filename="box_num"_$(date +"%m%d%y-%H%M%S")
 
 # Capture 3 hours of continuous video at 640x480 and 150kB/s bit rate into a pivideo.h264 file:
-raspivid -t 10000 -a "Box Number" -a 12 -w 640 -h 480 -fps 25 -b 1200000 -o /mnt/usbdisk/box_num.h264
+raspivid -t 10800000 -a "Box Number" -a 12 -w 640 -h 480 -fps 25 -b 1200000 -o /home/pi/$filename.h264
 
 # Wrap the raw video with an MP4 container: 
-MP4Box -add /mnt/usbdisk/box_num.h264 /mnt/usbdisk/box_num.mp4
+MP4Box -add /home/pi/$filename.h264 /home/pi/$filename.mp4
 
 # Remove the source raw file, leaving the remaining pivideo.mp4 file to play
-rm /mnt/usbdisk/box_num.h264
+rm /home/pi/$filename.h264
 
