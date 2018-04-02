2dd94bfe917329f15d9a622204db023aa161dab7
mpandit3 <meelyn.pandit@gmail.com>
2018-04-01 19:17:24 -0500

    Timestamp kept creating a new file for each minute, modified filename so it would only create one file for a given date.

--- temp.py
+++ temp.py
@@ -34,7 +34,7 @@ while True:
     #Open Log File
     now = datetime.datetime.now()
     timestamp = now.strftime("%m_%d_%Y %H:%M")
-    f=open('/home/pi/' + box_num + timestamp + '.txt','a')
+    f=open('/home/pi/' + box_num + now.strftime("%m_%d_%y") + '_temp.txt','a')
     outvalue = read_temp()
     data = outvalue, timestamp
     print(data)
