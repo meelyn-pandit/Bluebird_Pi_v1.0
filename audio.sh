4c5c17d4d64cf140d3df6416b241c5bf752e6a98
Meelyn M Pandit <meelyn.pandit@gmail.com>
2018-04-05 00:19:21 -0500

    Update audio.sh

--- audio.sh
+++ audio.sh
@@ -2,4 +2,4 @@
 
 filename="box_num"_$(date +"%m%d%y-%H%M%S")
 
-arecord -D plughw:1 -c1 -r 48000 -f S32_LE -t wav -V mono -d 10 --max-file-time 1800 -v /home/pi/$filename.wav
+arecord -D plughw:1 -c1 -r 48000 -f S32_LE -t wav -V mono -d 10 --max-file-time 1800 -v /mnt/usbdisk/$filename.wav
