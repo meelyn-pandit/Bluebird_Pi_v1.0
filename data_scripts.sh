cff706aff6bc352a805fc231b867b8b1973558d2
Meelyn M Pandit <meelyn.pandit@gmail.com>
2018-04-05 00:20:46 -0500

    Update data_scripts.sh

--- data_scripts.sh
+++ data_scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo sh /mnt/usbdisk/audio.sh &
-#sudo sh /mnt/usbdisk/video.sh &
+sudo sh /mnt/usbdisk/video.sh &
 sudo python /mnt/usbdisk/temp.py &
 sudo python /mnt/usbdisk/rpi_rfid.py &
 
