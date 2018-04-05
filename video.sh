3542c108409c74b5ed2f1f8cd789eb2ae301c52a
Meelyn M Pandit <meelyn.pandit@gmail.com>
2018-04-05 00:20:22 -0500

    Update video.sh

--- video.sh
+++ video.sh
@@ -3,11 +3,11 @@
 filename="box_num"_$(date +"%m%d%y-%H%M%S")
 
 # Capture 3 hours of continuous video at 640x480 and 150kB/s bit rate into a pivideo.h264 file:
-raspivid -t 10800000 -a "Box Number" -a 12 -w 640 -h 480 -fps 25 -b 1200000 -o /home/pi/$filename.h264
+raspivid -t 10800000 -a "Box Number" -a 12 -w 640 -h 480 -fps 25 -b 1200000 -o /mnt/usbdisk/$filename.h264
 
 # Wrap the raw video with an MP4 container: 
-MP4Box -add /home/pi/$filename.h264 /home/pi/$filename.mp4
+MP4Box -add /mnt/usbdisk/$filename.h264 /mnt/usbdisk/$filename.mp4
 
 # Remove the source raw file, leaving the remaining pivideo.mp4 file to play
-rm /home/pi/$filename.h264
+rm /mnt/usbdisk/$filename.h264
 
