8bae2730683164d70547a7b2fa2809e1f105f2cb
Meelyn M Pandit <meelyn.pandit@ou.edu>
2018-06-17 18:22:03 -0500

    Removed USB disk file paths
    .h264 and .mp4 files will be saved directly to the RPi SD card.

--- video.sh
+++ video.sh
@@ -4,11 +4,11 @@
 filename="box_num"_$(date +"%m%d%y-%H%M%S")
 
 # Capture 3 hours of continuous video at 640x480 and 150kB/s bit rate into a pivideo.h264 file:
-raspivid -t 10800000 -a "Box Number" -a 12 -w 640 -h 480 -fps 25 -b 1200000 -o /mnt/usbdisk/$filename.h264
+raspivid -t 10800000 -a "Box Number" -a 12 -w 640 -h 480 -fps 25 -b 1200000 -o $filename.h264
 
 # Wrap the raw video with an MP4 container: 
-MP4Box -add /mnt/usbdisk/$filename.h264 /mnt/usbdisk/$filename.mp4
+MP4Box -add $filename.h264 $filename.mp4
 
 # Remove the source raw file, leaving the remaining pivideo.mp4 file to play
-rm /mnt/usbdisk/$filename.h264
+rm $filename.h264
 
